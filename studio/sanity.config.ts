573707383d19cb20858a1752802994fbb463322f
tachakrit <chakrit.d@yahoo.com>
2025-08-12 22:09:43 +0700

    Create sanity.config.ts

---
+++ studio/sanity.config.ts
@@ -0,0 +1,13 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import schemas from './schemas'
+
+export default defineConfig({
+  name: 'drivetwo',
+  title: 'DriveTwo Studio',
+  projectId: 'vxcvflri',
+  dataset: 'production',
+  plugins: [structureTool(), visionTool()],
+  schema: { types: schemas }
+})
