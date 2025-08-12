e84064e1af4fbb31d830a48fa87284d40d557e9c
tachakrit <chakrit.d@yahoo.com>
2025-08-12 22:10:44 +0700

    Create brand.ts

---
+++ studio/schemas/types/brand.ts
@@ -0,0 +1,11 @@
+export default {
+  name: 'brand',
+  title: 'Brand (ยี่ห้อ)',
+  type: 'document',
+  fields: [
+    {name: 'name', title: 'ชื่อยี่ห้อ', type: 'string', validation: (Rule:any)=>Rule.required()},
+    {name: 'slug', title: 'Slug', type: 'slug', options: {source: 'name'}},
+    {name: 'country', title: 'ประเทศ', type: 'string'},
+    {name: 'logo', title: 'โลโก้', type: 'image'}
+  ]
+}
