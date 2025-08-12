3a7a8e68c309d2b88e5a7101458691953797c540
tachakrit <chakrit.d@yahoo.com>
2025-08-12 22:10:14 +0700

    Create index.ts

---
+++ studio/schemas/index.ts
@@ -0,0 +1,6 @@
+import brand from './types/brand'
+import carModel from './types/carModel'
+import review from './types/review'
+import news from './types/news'
+
+export default [brand, carModel, review, news]
