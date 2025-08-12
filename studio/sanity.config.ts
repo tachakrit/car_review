4f8427153911031f44bd22ef54b3be1aed41add0
tachakrit <chakrit.d@yahoo.com>
2025-08-12 22:28:29 +0700

    Update sanity.config.ts

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -6,7 +6,7 @@ import schemas from './schemas'
 export default defineConfig({
   name: 'drivetwo',
   title: 'DriveTwo Studio',
-  projectId: 'YOUR_PROJECT_ID', // <-- replace with your real Project ID
+  projectId: 'vxcvflri', // <-- replace with your real Project ID
   dataset: 'production',
   plugins: [structureTool(), visionTool()],
   schema: { types: schemas }
