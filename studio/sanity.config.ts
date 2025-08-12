04074beb6b0c2f3051f495b605b822c8f1b414f6
tachakrit <chakrit.d@yahoo.com>
2025-08-12 22:27:16 +0700

    Add files via upload

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -6,7 +6,7 @@ import schemas from './schemas'
 export default defineConfig({
   name: 'drivetwo',
   title: 'DriveTwo Studio',
-  projectId: 'vxcvflri',
+  projectId: 'YOUR_PROJECT_ID', // <-- replace with your real Project ID
   dataset: 'production',
   plugins: [structureTool(), visionTool()],
   schema: { types: schemas }
